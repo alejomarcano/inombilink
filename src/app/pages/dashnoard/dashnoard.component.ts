8cce3cc35db1757949408eb8b8b63c0d58bb6c60
Alejandro Marcano Van Grieken <alemvangrieken@gmail.com>
2018-04-28 13:49:41 -0400

    Primer commit login/registro, barra de nav y sidebar

---
+++ src/app/pages/dashnoard/dashnoard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashnoard',
+  templateUrl: './dashnoard.component.html',
+  styles: []
+})
+export class DashnoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
